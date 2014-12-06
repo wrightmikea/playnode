e23f22be08c610b309f7acb2ac485759d9837922
liz w <liz@bugmenot.com>
2014-12-06 13:22:02 -0800

    rest server

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+var express = require('express');
 var Faker = require('Faker');
 var randomName = Faker.Name.findName(); // Rowan Nikolaus
 var randomEmail = Faker.Internet.email(); // Kassandra.Haley@erich.biz
@@ -6,4 +7,14 @@ var rn=Math.random();
 var rn2=Math.random();
 var newrn = rn * rn2;
 console.log (newrn);
+var app = express();
+var port = process.env.PORT || 8080;
+var router = express.Router();
+router.get('/',function(req,res) {
+    res.send('hi');
+});
+app.use('/api', router);
+app.listen(port);
+console.log('listening on ' + port);
+
 
