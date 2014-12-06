e6d9f4f4898e75c58826a9735ba8342e4ebbd0e3
liz w <liz@bugmenot.com>
2014-12-06 13:42:17 -0800

    saved users

--- src/app.js
+++ src/app.js
@@ -1,17 +1,22 @@
+
 var express = require('express');
 var Faker = require('Faker');
+var userSave = require('save')('user');
 var randomName = Faker.Name.findName(); // Rowan Nikolaus
 var randomEmail = Faker.Internet.email(); // Kassandra.Haley@erich.biz
 console.log("HI " + randomName + "(" + randomEmail + ")");
 var rn=Math.random();
-var rn2=Math.random();
-var newrn = rn * rn2;
-console.log (newrn);
+console.log (rn);
+userSave.create({name: Faker.Name.findName()});
+userSave.create({name: Faker.Name.findName()});
 var app = express();
 var port = process.env.PORT || 8080;
 var router = express.Router();
 router.get('/',function(req,res) {
-    res.send('hi');
+    //res.send('hi');
+    userSave.find({}, function(error, users) {
+        res.send(users);
+    })
 });
 app.use('/api', router);
 app.listen(port);
