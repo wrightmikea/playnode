0a776dfdfc3884bd9a5a2b0bb5355efc283f13d1
Mike Wright <wrightmikea@gmail.com>
2014-12-07 13:33:12 -0800

    mongoose

--- README.md
+++ README.md
@@ -9,16 +9,19 @@ to experiment with node modules
 setup
 -----
 
-1. clone this repo
-2. checkout the develop branch
-3. (Linux) sudo apt-get install build-essential
-4. npm install 
+1. install mongodb, npm, and gulp
+2. clone this repo
+3. checkout the develop branch
+4. (Linux) sudo apt-get install build-essential
+5. npm install 
 
 usage
 -----
 
-0. gulp
-1. npm test
-2. node src/app.js
-3. browse localhost:8080/api
+1. start mongo db server (mongod)
+2. lint and build (gulp)
+3. run tests (npm test)
+4. run express app (node src/app.js)
+5. invoke rest endpoint (browse localhost:8080/api)
+6. verify database (run mongo; use playnode; db.cats.find())
 
--- src/app.js
+++ src/app.js
@@ -1,14 +1,31 @@
 
 var express = require('express');
 var Faker = require('Faker');
+var mongoose = require('mongoose');
 var userSave = require('save')('user');
 var randomName = Faker.Name.findName(); // Rowan Nikolaus
 var randomEmail = Faker.Internet.email(); // Kassandra.Haley@erich.biz
 console.log("HI " + randomName + "(" + randomEmail + ")");
 var rn=Math.random();
 console.log (rn);
-userSave.create({name: Faker.Name.findName()});
-userSave.create({name: Faker.Name.findName()});
+var names = [ Faker.Name.findName(), Faker.Name.findName() ];
+userSave.create({name: names[0]});
+userSave.create({name: names[1]});
+
+mongoose.connect('mongodb://localhost/playnode');
+var Cat = mongoose.model('Cat', { name: String });
+var kittyErr = function (err) {
+    if (err) {
+        console.log("kittyErr(" + err + ')');
+    }
+};
+
+var kitty = new Cat({ name: names[0] });
+kitty.save(kittyErr);
+
+kitty = new Cat({ name: names[1] });
+kitty.save(kittyErr);
+
 var app = express();
 var port = process.env.PORT || 8080;
 var router = express.Router();
@@ -21,5 +38,3 @@ router.get('/',function(req,res) {
 app.use('/api', router);
 app.listen(port);
 console.log('listening on ' + port);
-
-
